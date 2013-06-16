1b6946a605a605b2e811ba3947142a263bac1240
Youngdeok Kim <youngdeok.k@gmail.com>
2013-06-17 02:44:00 +0900

    Listener 삭제

---
+++ src/main/java/org/herring/cruiser/core/service/work/AgentListener.java
@@ -0,0 +1,17 @@
+package org.herring.cruiser.core.service.work;
+
+import org.herring.cruiser.core.network.NextWorker;
+import org.herring.cruiser.core.request.Request;
+
+/**
+ * Description.
+ *
+ * @author Youngdeok Kim
+ * @since 1.0
+ */
+public class AgentListener extends Work {
+    @Override
+    public void working(Request request, NextWorker nextWorker) {
+
+    }
+}
