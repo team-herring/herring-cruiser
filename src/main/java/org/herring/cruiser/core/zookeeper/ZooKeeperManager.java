64bbe73d7d87e7913be10cb27262b65c70ec9079
Youngdeok Kim <youngdeok.k@gmail.com>
2013-06-17 03:25:31 +0900

    zookeeper에 필요한 설정 파일 추가

--- src/main/java/org/herring/cruiser/core/zookeeper/ZooKeeperManager.java
+++ src/main/java/org/herring/cruiser/core/zookeeper/ZooKeeperManager.java
@@ -1,11 +1,10 @@
 package org.herring.cruiser.core.zookeeper;
 
+import org.herring.core.cluster.zookeeper.ZookeeperClient;
 import org.herring.cruiser.job.Group;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.util.*;
 
 /**
  * Description.
@@ -16,10 +15,26 @@ import java.util.Map;
 public class ZooKeeperManager {
     public static final String TOPOLOGY_DIRECTORY = "/topology";
     public static final String EVENT_DIRECTORY = "/event";
-    private static final String BASE_PATH="cruiser/jobs/";
-    private static Map<String, List<JobEvent>> eventMap;
+    private static final String BASE_PATH = "cruiser/jobs/";
+    private static final Map<String, List<JobEvent>> eventMap;
+    private static ZookeeperClient zkClient;
+
     static {
         eventMap = new HashMap<String, List<JobEvent>>();
+
+        Properties properties = new Properties();
+        try {
+            properties.load(ClassLoader.getSystemResourceAsStream("zookeeper.properties"));
+            String uri = properties.getProperty("zookeeper.server-uri");
+            int timeout = Integer.valueOf(properties.getProperty("zookeeper.timeout"));
+
+            ArrayList<Boolean> watchCheck = new ArrayList<Boolean>();
+            for (int i = 0; i < 3; ++i)
+                watchCheck.add(false);
+            zkClient = new ZookeeperClient(uri, timeout, ClassLoader.getSystemResource("jass.conf").toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void registJob(int jobID) {
@@ -30,16 +45,16 @@ public class ZooKeeperManager {
 
     }
 
-    public static void createFile(String path){
+    public static void createFile(String path) {
 
     }
 
-    public static void createFolder(String path){
+    public static void createFolder(String path) {
 
     }
 
-    public static void addListener(String path, JobEvent event){
-        if ( !eventMap.containsKey(path))
+    public static void addListener(String path, JobEvent event) {
+        if (!eventMap.containsKey(path))
             eventMap.put(path, new ArrayList<JobEvent>());
         List<JobEvent> events = eventMap.get(path);
         events.add(event);
@@ -47,7 +62,7 @@ public class ZooKeeperManager {
         //주키퍼 등록
     }
 
-    public static void remove(String path){
+    public static void remove(String path) {
         eventMap.remove(path);
     }
 }
