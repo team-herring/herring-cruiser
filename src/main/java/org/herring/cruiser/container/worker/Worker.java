2a12633b332a42a1f5ded22c3abcfdd47fe41ddd
Youngdeok Kim <youngdeok.k@gmail.com>
2013-06-16 19:31:33 +0900

    불필요한 부분 주석 처리 및 기능 변경

--- src/main/java/org/herring/cruiser/container/worker/Worker.java
+++ src/main/java/org/herring/cruiser/container/worker/Worker.java
@@ -1,13 +1,6 @@
 package org.herring.cruiser.container.worker;
 
-import org.herring.cruiser.core.event.EventHandler;
-import org.herring.cruiser.core.model.JobCommand;
-import org.herring.cruiser.core.network.MessageSender;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Description.
@@ -18,10 +11,12 @@ import java.nio.ByteBuffer;
 public class Worker {
     private String ip;
     private int port;
+    private AtomicInteger jobCount;
 
     public Worker(String ip, int port) {
         this.ip = ip;
         this.port = port;
+        this.jobCount = new AtomicInteger(1);
     }
 
     public String getIp() {
@@ -32,22 +27,71 @@ public class Worker {
         return port;
     }
 
-    public void takeWork(JobCommand jobCommand, EventHandler eventHandler) throws IOException {
-        jobCommand.setNextServerIP(ip);
-        jobCommand.setNextServerPort(port);
 
+//    public void takeWork(JobCommand jobCommand, EventHandler eventHandler) throws IOException {
+//        jobCommand.setNextServerIP(ip);
+//        jobCommand.setNextServerPort(port);
+//
+//
+//        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+//        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+//        objectOutputStream.writeObject(jobCommand);
+//
+//        ByteBuffer buffer = ByteBuffer.wrap(outputStream.toByteArray());
+//        MessageSender sender = new MessageSender(ip, port);
+//        sender.send(buffer, eventHandler);
+//    }
+//
+//    public void nextWorker(Worker worker) {
+//        this.ip = worker.getIp();
+//        this.port = worker.getPort();
+//    }
+//
+//    public JobCommand createJob(Integer serviceCommand) throws IOException {
+//        JobCommand jobCommand = JobCommandManager.createAndRegist(serviceCommand);
+//        takeWork(jobCommand, new EventHandler() {
+//            @Override
+//            public void handler(NetworkContext context, Object o) {
+//            }
+//        });
+//        return jobCommand;
+//    }
+//
+//    public void destroyJob(final int jobId) throws IOException {
+//        JobCommand jobCommand = JobCommandManager.find(jobId);
+//        jobCommand.setCommand(Commands.WORKER_DESTROY.getValue());
+//        takeWork(jobCommand, new EventHandler() {
+//            @Override
+//            public void handler(NetworkContext context, Object o) {
+//                JobCommandManager.remove(jobId);
+//            }
+//        });
+//    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-        objectOutputStream.writeObject(jobCommand);
+        Worker worker = (Worker) o;
 
-        ByteBuffer buffer = ByteBuffer.wrap(outputStream.toByteArray());
-        MessageSender sender = new MessageSender(ip, port);
-        sender.send(buffer, eventHandler);
+        if (jobCount != worker.jobCount) return false;
+        if (port != worker.port) return false;
+        if (ip != null ? !ip.equals(worker.ip) : worker.ip != null) return false;
+
+        return true;
     }
 
-    public void nextWorker(Worker worker) {
-        this.ip = worker.getIp();
-        this.port = worker.getPort();
+    @Override
+    public int hashCode() {
+        int result = ip != null ? ip.hashCode() : 0;
+        result = 31 * result + port;
+        return result;
+    }
+
+    public void decrease() {
+        jobCount.getAndDecrement();
+    }
+    public void increase(){
+        jobCount.getAndIncrement();
     }
 }
