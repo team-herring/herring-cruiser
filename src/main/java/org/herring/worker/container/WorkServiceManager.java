424a04e19e4885517c9f1dcbb010c011d3de7408
Youngdeok Kim <youngdeok.k@gmail.com>
2013-06-17 03:54:02 +0900

    WorkerService 변경

--- src/main/java/org/herring/worker/container/WorkServiceManager.java
+++ src/main/java/org/herring/worker/container/WorkServiceManager.java
@@ -1,5 +1,6 @@
 package org.herring.worker.container;
 
+import org.herring.cruiser.core.model.JobCommand;
 import org.herring.cruiser.core.request.Request;
 import org.herring.cruiser.core.service.WorkerService;
 
@@ -13,21 +14,29 @@ import java.util.concurrent.ConcurrentHashMap;
  * @since 1.0
  */
 public class WorkServiceManager {
-    private static Map<Request, WorkerService> workerServiceMap;
+    private static Map<JobCommand, WorkerService> workerServiceMap;
 
     static {
-        workerServiceMap = new ConcurrentHashMap<Request, WorkerService>();
+        workerServiceMap = new ConcurrentHashMap<JobCommand, WorkerService>();
     }
 
     public static WorkerService findAndCreate(Request request) {
-        if (workerServiceMap.containsKey(request))
-            return workerServiceMap.get(request);
-        WorkerService workerService = WorkerServiceFactory.create(request);
-        workerServiceMap.put(request, workerService);
+        JobCommand jobCommand = (JobCommand) request.getData();
+        assert (jobCommand != null);
+
+        if (workerServiceMap.containsKey(jobCommand))
+            return workerServiceMap.get(jobCommand);
+
+        WorkerService workerService = WorkerServiceFactory.create(jobCommand);
+        if (workerService == null)
+            throw new RuntimeException("does not create WorkerService");
+
+        workerServiceMap.put(jobCommand, workerService);
         return workerService;
     }
 
     public static void remove(Request request){
-        workerServiceMap.remove(request);
+        JobCommand jobCommand = (JobCommand) request.getData();
+        workerServiceMap.remove(jobCommand);
     }
 }
--- src/main/java/org/herring/worker/container/WorkerServiceFactory.java
+++ src/main/java/org/herring/worker/container/WorkerServiceFactory.java
@@ -1,11 +1,8 @@
 package org.herring.worker.container;
 
 import org.apache.log4j.Logger;
-import org.herring.cruiser.core.request.Request;
+import org.herring.cruiser.core.model.JobCommand;
 import org.herring.cruiser.core.service.WorkerService;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Description.
@@ -15,15 +12,9 @@ import java.util.Map;
  */
 public class WorkerServiceFactory {
     private static final Logger log = Logger.getLogger(WorkerServiceFactory.class);
-    private static Map<Integer, String> workerService;
 
-    static {
-        workerService = new HashMap<Integer, String>();
-        workerService.put(1, "org.herring.worker.server.service.imple.RegistJobWorker");
-    }
-
-    public static WorkerService create(Request request) {
-        String className = workerService.get(request.getCommand());
+    public static WorkerService create(JobCommand jobCommand) {
+        String className = jobCommand.getServiceName();
         try {
             return (WorkerService) Class.forName(className).newInstance();
         } catch (Exception ignored){
