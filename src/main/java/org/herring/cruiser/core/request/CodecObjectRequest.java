571661ab1c47f3df9aad19bb142e2362cc1cdd4f
Youngdeok Kim <youngdeok.k@gmail.com>
2013-06-17 03:41:44 +0900

    불필요한 stepID 삭제

--- src/main/java/org/herring/cruiser/core/request/CodecObjectRequest.java
+++ src/main/java/org/herring/cruiser/core/request/CodecObjectRequest.java
@@ -34,40 +34,12 @@ public class CodecObjectRequest implements Request<JobCommand> {
         return jobCommand.getCommand();
     }
 
-    @Override
     public int getJobID() {
         return jobID;
     }
 
     @Override
-    public int getStepID() {
-        return stepID;
-    }
-
-    @Override
     public JobCommand getData() {
         return this.jobCommand;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        CodecObjectRequest that = (CodecObjectRequest) o;
-
-        if (jobID != that.jobID) return false;
-        if (serviceID != that.serviceID) return false;
-        if (stepID != that.stepID) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = jobID;
-        result = 31 * result + stepID;
-        result = 31 * result + serviceID;
-        return result;
     }
 }
