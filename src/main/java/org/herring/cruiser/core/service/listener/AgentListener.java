f20a3dc790f7432090f0b0b1ff8716c00aa1ac05
Youngdeok Kim <youngdeok.k@gmail.com>
2013-06-17 02:37:56 +0900

    AgentListener 추가

--- src/main/java/org/herring/cruiser/core/service/listener/AgentListener.java
+++ src/main/java/org/herring/cruiser/core/service/listener/AgentListener.java
@@ -2,10 +2,7 @@ package org.herring.cruiser.core.service.listener;
 
 import org.herring.cruiser.core.network.NextWorker;
 import org.herring.cruiser.core.request.Request;
-import org.herring.cruiser.core.response.Response;
-import org.herring.cruiser.core.service.WorkerService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,21 +11,9 @@ import java.util.List;
  * @author Youngdeok Kim
  * @since 1.0
  */
-public abstract class AgentListener<T> implements WorkerService {
-    protected List<T> values;
-
-    protected AgentListener() {
-        this.values = new ArrayList<T>();
-        create(values);
-    }
-
+public class AgentListener extends Listener {
     @Override
-    public void service(Request request, Response response, NextWorker nextWorker) {
-        List<T> results = listen(request, values, nextWorker);
-        values.addAll(results);
+    public List listen(Request request, List values, NextWorker nextWorker) {
+        return null;
     }
-
-    public void create(List<T> values){
-    }
-    public abstract List<T> listen(Request request, List<T> values, NextWorker nextWorker);
 }
