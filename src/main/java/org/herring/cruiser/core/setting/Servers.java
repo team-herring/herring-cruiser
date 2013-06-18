4ab350fdc0436c0fd4b963d3c7ada5b6c179bfc8
Youngdeok Kim <youngdeok.k@gmail.com>
2013-06-19 08:50:04 +0900

    서버 정보 추가

---
+++ src/main/java/org/herring/cruiser/core/setting/Servers.java
@@ -0,0 +1,12 @@
+package org.herring.cruiser.core.setting;
+
+/**
+ * Description.
+ *
+ * @author Youngdeok Kim
+ * @since 1.0
+ */
+public class Servers {
+    public static final String CRUISER_IP = "localhost";
+    public static final int CRUISER_PORT = 9928;
+}
